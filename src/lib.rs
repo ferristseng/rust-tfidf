db6a5c57006fcd109065960584497fd7a2de8b38
Ferris Tseng <ferristseng@fastmail.fm>
2016-02-03 10:15:14 -0500

    Add license headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2016 rust-tfidf Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 //! Library to calculate TF-IDF (Term Frequency - Inverse Document Frequency)
 //! for generic documents. The library provides strategies to act on objects
 //! that implement certain document traits (`NaiveDocument`, `ProcessedDocument`,
